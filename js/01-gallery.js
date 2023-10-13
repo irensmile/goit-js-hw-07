cce63518706158ff62999aee921f60491c59231c
irensmile <curkan.ira@gmail.com>
2023-10-13 13:22:44 -0400

    1-завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+galleryItems.forEach((item) => {
+    const liItem = document.createElement("li");
+    liItem.innerHTML = `<a class="gallery__link" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />`
+    galleryList.append(liItem);
+});
+
+const imageLinks = document.querySelectorAll('.gallery__link');
+imageLinks.forEach((imageLink) => {
+    imageLink.addEventListener('click', function (event) {
+        event.preventDefault(); // Скасовуємо стандартну дію посилання
+    
+    const instance = basicLightbox.create(`<img src=${event.target.dataset.source}>`)
+    instance.show()
+    });
+})
+//console.log(galleryItems);
