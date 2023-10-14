3b36e2f5520857bc9040440b6640e7c1b40c1fca
irensmile <curkan.ira@gmail.com>
2023-10-14 15:28:26 -0400

    2 завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+
 const galleryList = document.querySelector(".gallery");
-galleryItems.forEach((item) => {
+
+//let liItems = [];
+//galleryItems.forEach((item) => {
+//     const liItem = document.createElement("li");
+//     liItem.innerHTML = `<a class="gallery__link" href=${item.original}>
+//     <img
+//       class="gallery__image"
+//       src=${item.preview}
+//       data-source=${item.original}
+//       alt=${item.description}
+//     />`
+//     liItems.push(liItem);
+//     //galleryList.append(liItem);
+// });
+
+const liItems = galleryItems.map((item) => {
     const liItem = document.createElement("li");
+    liItem.classList.add('gallery__item');
     liItem.innerHTML = `<a class="gallery__link" href=${item.original}>
     <img
       class="gallery__image"
@@ -11,16 +29,25 @@ galleryItems.forEach((item) => {
       data-source=${item.original}
       alt=${item.description}
     />`
-    galleryList.append(liItem);
+    return liItem;
 });
 
-const imageLinks = document.querySelectorAll('.gallery__link');
-imageLinks.forEach((imageLink) => {
-    imageLink.addEventListener('click', function (event) {
-        event.preventDefault(); // Скасовуємо стандартну дію посилання
+galleryList.append(...liItems);
+
+// const imageLinks = document.querySelectorAll('.gallery__link');
+// imageLinks.forEach((imageLink) => {
+//      imageLink.addEventListener('click', function (event) {
+// //         event.preventDefault(); // Скасовуємо стандартну дію посилання
     
+//         const instance = basicLightbox.create(`<img src=${event.target.dataset.source}>`)
+//         instance.show()
+//     });
+// })
+
+galleryList.addEventListener('click', function (event) {
+    event.preventDefault(); // Скасовуємо стандартну дію посилання
     const instance = basicLightbox.create(`<img src=${event.target.dataset.source}>`)
     instance.show()
-    });
-})
+}); //підписуємся на подію click для батьківського елементу ul замість "а"шок
+
 //console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryList = document.querySelector(".gallery");
+
+const liItems = galleryItems.map((item) => {
+    const liItem = document.createElement("li");
+    liItem.classList.add('gallery__item');
+    liItem.innerHTML = `<a class="gallery__link" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      alt=${item.description}
+    />`
+    return liItem;
+});
+
+galleryList.append(...liItems);
+
+// const imageLinks = document.querySelectorAll('.gallery__link');
+// imageLinks.forEach((imageLink) => {
+//     imageLink.addEventListener('click', function (event) {
+//         event.preventDefault(); // Скасовуємо стандартну дію посилання
+//     });
+// });
+
+
+(function () {
+    let gallery = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionDelay: 250
+    })
+})();
+
